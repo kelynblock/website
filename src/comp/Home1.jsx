03742389f5ce97b811b1af8d8186f75d3ed0ef49
kelynblock <kelynblock@gmail.com>
2023-08-07 00:05:06 -0400

    2nd commit

--- src/comp/Home1.jsx
+++ src/comp/Home1.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import './home1.css';
+import "./home1.css";
+import { Link } from "react-router-dom";
 
+import { PiPencilCircleBold } from "react-icons/pi";
+import { FaReact } from "react-icons/fa";
 
 const Home1 = () => {
   return (
@@ -11,6 +14,59 @@ const Home1 = () => {
             <div className="im_container">
               <div className="img_box">
                 <img src="img/me.png" alt="me"></img>
+              </div>
+            </div>
+          </div>
+          <Link to="/about" className="link">
+            <div className="deatail_container">
+              <h4>a web designer</h4>
+              <h3> Kelyn Block</h3>
+              <p>I'm an Indianapolis-based web developer</p>
+              <div className="icon">
+                <AiOutlineDoubleRight />
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div className="right_container">
+          <div className="content">
+            <div className="top_box">
+              <p>latest works and features</p>
+            </div>
+            <div className="bottom_box">
+              <div className="box">
+                <div className="img_box">
+                  <img className="img/my-works.png" alt="work"></img>
+                </div>
+                <Link to="/about" className="link">
+                  <div className="detail">
+                    <div className="info">
+                      <p>showcase</p>
+                      <h3>projects</h3>
+                    </div>
+                    <div className="icon">
+                      <AiOutlineDoubleRight />
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mid_container">
+          <div className="content">
+            <div className="box">
+              <AiOutlineCamera />
+              <PiPencilCircleBold />
+              <FaReact />
+            </div>
+            <div className="detail">
+              <div className="info">
+                <p>blog</p>
+                <h3>fonts</h3>
+              </div>
+              <div className="icon">
+                <AiOutlineDoubleRight />
               </div>
             </div>
           </div>
