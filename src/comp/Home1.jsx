00acb6e6f95e1ad0d4ff9a2d5fc01f23b8ad1e93
kelynblock <kelynblock@gmail.com>
2023-08-11 23:20:03 -0400

    5th commit

--- src/comp/Home1.jsx
+++ src/comp/Home1.jsx
@@ -4,72 +4,74 @@ import { Link } from "react-router-dom";
 import {
   AiOutlineDoubleRight,
   AiOutlineCamera,
-  AiOutlineDribble,
+  AiOutlineDribbble,
   AiOutlineTwitter,
 } from "react-icons/ai";
 import { PiPencilCircleBold } from "react-icons/pi";
 import { FaReact } from "react-icons/fa";
 import { BiCodeAlt } from "react-icons/bi";
-import Nav from "./Nav";
-
 const Home1 = () => {
   return (
     <>
-      <Nav />
       <div className="home">
         <div className="top_container">
           <div className="left_container">
-            <div className="im_container">
+            <div className="img_container">
               <div className="img_box">
                 <img src="img/me.png" alt="me"></img>
               </div>
             </div>
+            <Link to="/about" className="link">
+              <div className="detail_container">
+                <h4>a React Developer</h4>
+                <h3>Kelyn Block</h3>
+                <p>
+                  I am a Indianapolis-based React Developer and web designer
+                </p>
+                <div className="icon">
+                  <AiOutlineDoubleRight />
+                </div>
+              </div>
+            </Link>
           </div>
-          <Link to="/about" className="link">
-            <div className="deatail_container">
-              <h4>a web designer</h4>
-              <h3> Kelyn Block</h3>
-              <p>I'm an Indianapolis-based web developer</p>
-              <div className="icon">
-                <AiOutlineDoubleRight />
+          <div className="right_container">
+            <div className="content">
+              <div className="top_box">
+                <p>latest works and featured</p>
               </div>
-            </div>
-          </Link>
-        </div>
-        <div className="right_container">
-          <div className="content">
-            <div className="top_box">
-              <p>latest works and features</p>
-            </div>
-            <div className="bottom_box">
-              <div className="box">
-                <div className="img_box">
-                  <img className="img/my-works.png" alt="work"></img>
-                </div>
-                <Link to="/about" className="link">
-                  <div className="detail">
-                    <div className="info">
-                      <p>more about me</p>
-                      <h3>credentials</h3>
-                    </div>
-                    <div className="icon">
-                      <AiOutlineDoubleRight />
-                    </div>
+              <div className="bottom_box">
+                <div className="box">
+                  <div className="img_box">
+                    <img src="img/sign.png" alt="sign"></img>
                   </div>
-                </Link>
-              </div>
-              <div className="box">
-                <div className="img_box">
-                  <img src="img/my-works.png" alt="work"></img>
-                </div>
-                <Link to="/about" className="link">
-                  <div className="detail">
-                    <div className="info">
-                      <p>showcase</p>
-                      <h3>projects</h3>
+                  <Link to="/about" className="link">
+                    <div className="detail">
+                      <div className="info">
+                        <p>more about me</p>
+                        <h3>credentials</h3>
+                      </div>
+                      <div className="icon">
+                        <AiOutlineDoubleRight />
+                      </div>
                     </div>
+                  </Link>
+                </div>
+                <div className="box">
+                  <div className="img_box">
+                    <img src="img/my-works.png" alt="work"></img>
                   </div>
-                </Link>
+                  <Link to="/work" className="link">
+                    <div className="detail">
+                      <div className="info">
+                        <p>showcase</p>
+                        <h3>projects</h3>
+                      </div>
+                      <div className="icon">
+                        <AiOutlineDoubleRight />
+                      </div>
+                    </div>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
@@ -102,9 +104,9 @@ const Home1 = () => {
                 </div>
               </div>
               <Link to="/services" className="link">
-                <div className="detal">
+                <div className="detail">
                   <div className="info">
-                    <p>spcecialization</p>
+                    <p>specialiazation</p>
                     <h3>services offering</h3>
                   </div>
                   <div className="icon">
@@ -113,11 +115,11 @@ const Home1 = () => {
                 </div>
               </Link>
             </div>
-            <div className="box">
+            <div className="box3">
               <div className="img_box">
-                <div className=" img_container">
+                <div className="img_container">
                   <div className="img">
-                    <AiOutlineDribble />
+                    <AiOutlineDribbble />
                   </div>
                   <div className="img">
                     <AiOutlineTwitter />
